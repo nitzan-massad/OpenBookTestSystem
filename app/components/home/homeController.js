f91a1519002ef19d931cfa292c9484fd6b84db91
nitzan <nitzanmassad@gmail.com>
2018-04-16 19:55:28 +0300

    16.4.18

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,15 +1,18 @@
 
-app.controller('homeController', ['$location', '$window',
-    function($location, $window) {
+app.controller('homeController', ['$location', '$window','coursePageService',
+    function($location, $window,coursePageService) {
         let self=this;
         self.name = "Nitzan";
-        self.image ="folderImg.png";
+        self.image ="styles/folderImg.png";
         self.courses = [
             {courseName:'Advanced topics in cyber security', courseNumber:'372-0-000',courseTestDateFirst:'12.1.18',courseTestDateSecond:'12.2.18'},
             {courseName:'Algorythms', courseNumber:'372-0-111',courseTestDateFirst:'11.1.18',courseTestDateSecond:'11.2.18'},
             {courseName:'Information Retrieval', courseNumber:'372-0-222',courseTestDateFirst:'1.1.18',courseTestDateSecond:'1.2.18'}
         ];
-        self.count =0;
 
+        self.openCoursePage = function (courseID) {
+            coursePageService.setCourseID(courseID)
+            window.location.href = "#/coursePage";
+        }
     }
     ]);
