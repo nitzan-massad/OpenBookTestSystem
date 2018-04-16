ac38d6b878cfd52557b4a3832d0f22603f0c6cea
nitzan <nitzanmassad@gmail.com>
2018-04-16 18:32:48 +0300

    16.4.18

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -3,13 +3,13 @@ app.controller('homeController', ['$location', '$window',
     function($location, $window) {
         let self=this;
         self.name = "Nitzan";
+        self.image ="folderImg.png";
         self.courses = [
-            {name:'Advanced topics in cyber security', number:'372-0-000'},
-            {name:'Algorythms', number:'372-0-111'},
-            {name:'Information Retrieval', number:'372-0-222'}
+            {courseName:'Advanced topics in cyber security', courseNumber:'372-0-000',courseTestDateFirst:'12.1.18',courseTestDateSecond:'12.2.18'},
+            {courseName:'Algorythms', courseNumber:'372-0-111',courseTestDateFirst:'11.1.18',courseTestDateSecond:'11.2.18'},
+            {courseName:'Information Retrieval', courseNumber:'372-0-222',courseTestDateFirst:'1.1.18',courseTestDateSecond:'1.2.18'}
         ];
-        console.log("avi");
-        // self.avi = "avi ron";
+        self.count =0;
 
     }
     ]);
